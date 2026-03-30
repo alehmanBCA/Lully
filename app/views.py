0a96b809be42d6e3b34125a9b4bea98b148454ae
aiden <alehman26@basecampcodingacademy.org>
2026-03-30 09:02:28 -0500

    Commit to swtich branch

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            auth_login(request, user)
+            return redirect('home')
+    return render(request, 'signup.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'login.html')

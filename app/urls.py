02bd85b83d326ec5f38a811b9b038ccbb8aa2634
AngelSSr69 <angelsalazar43215@gmail.com>
2026-04-02 11:14:59 -0500

    Angel's Stuff

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
+    
+    path('monitor/<int:baby_id>/', views.monitor_dashboard, name='monitor_dashboard'),
+    path('api/baby/<int:baby_id>/vitals/', views.api_latest_vitals, name='api_vitals'),
+    # path('api/baby/<int:baby_id>/history/', views.baby_history_api, name='api_history'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -4,12 +4,33 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Baby, HealthReading, DeviceStatus
+from .forms import BabyForm
+
 # Create your views here.
 def home(request):
     return render(request, 'dashboard.html')
 
+# def profile(request):
+#     return render(request, 'profile.html')
+
+
 def profile(request):
-    return render(request, 'profile.html')
+    if request.method == 'POST':
+        form = BabyForm(request.POST)
+        if form.is_valid():
+            new_baby = form.save(commit=False)
+            new_baby.parent = request.user
+            new_baby.save()
+            return redirect('profile')
+    
+    babies = Baby.objects.filter(parent=request.user)
+    form = BabyForm()
+    return render(request, 'profile.html', {'babies': babies, 'form': form})
+
+
 
 def signup(request):
     if request.method == 'POST':
@@ -34,3 +55,27 @@ def login_view(request):
         else:
             messages.error(request, 'Invalid credentials')
     return render(request, 'login.html')
+
+
+
+def monitor_dashboard(request, baby_id):
+    baby = get_object_or_404(Baby, id=baby_id)
+    latest_vitals = baby.readings.order_by('-timestamp').first()
+    device = DeviceStatus.objects.filter(baby=baby).first()
+    
+    return render(request, 'monitor.html', {
+        'baby': baby,
+        'vitals': latest_vitals,
+        'device': device
+    })
+
+def api_latest_vitals(request, baby_id):
+    baby = get_object_or_404(Baby, id=baby_id)
+    latest = baby.readings.order_by('-timestamp').first()
+    
+    return JsonResponse({
+        "heart_rate": latest.heart_rate if latest else "--",
+        "oxygen": latest.oxygen_level if latest else "--",
+        "temp": float(latest.baby_temperature) if latest else "--",
+        "status": latest.sleep_status if latest else "Unknown"
+    })

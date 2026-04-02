34731dffeab9771081e6e275ef311f8459c8baf2
possibly-nai <nfarr26@basecampcodingacademy.org>
2026-04-02 09:53:21 -0500

    pushing

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # During development, also look for static files in the `src/` folder
 from pathlib import Path as _Path
